4fddf54a8fdcdee16462d8afa94a5334c92ef5a2
Olena Savelieva <o.savelieva@studytube.nl>
2018-12-24 12:17:19 +0200

    Add for loop to setup-hooks script

--- setup-hooks.sh
+++ setup-hooks.sh
@@ -4,13 +4,11 @@ YELLOW='\033[0;33m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-SRC_DIR='./node_modules/ng-git-hooks/hooks/'
-DEST_DIR='./.git/hooks/'
-
 if [[ -d .git ]]; then
-  cp --remove-destination "${SRC_DIR}pre-push.sh" "${DEST_DIR}pre-push";
-  cp --remove-destination "${SRC_DIR}pre-commit.sh" "${DEST_DIR}pre-commit";
-  cp --remove-destination "${SRC_DIR}post-merge.sh" "${DEST_DIR}post-merge";
+  for hook in pre-push pre-commit post-merge
+  do
+    cp --remove-destination "./node_modules/ng-git-hooks/hooks/${hook}.sh" "./.git/hooks/${hook}";
+  done
   echo -e "${GREEN}Success adding git hooks${NC}"
 else
   echo -e "${YELLOW}Failure adding git hooks: .git/ directory not found${NC}"
