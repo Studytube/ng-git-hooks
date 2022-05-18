bc07af19c88f322d93199ea7ba14687d17723b5b
Remco van 't Veer <remco@remworks.net>
2022-05-18 11:30:06 +0200

    Make scripts more POSIX shell compliant
    Bash is not available at /bin/bash on all systems and might even be
    absent.  The changes allow running these scripts on more systems.

--- hooks/pre-push.sh
+++ hooks/pre-push.sh
@@ -1,22 +1,21 @@
-#!/bin/bash
+#!/bin/sh
 
-[[ -z "$GIT_DIR" ]] && GIT_DIR=".";
-CHANGES=$(git status --porcelain);
-if [[ ! -z "$CHANGES" ]];
-then
-  echo "==> Stash local changes";
-  git stash --quiet -u;
-fi;
+[ -z "$GIT_DIR" ] && GIT_DIR="."
 
-if [[ $(npm run | grep "^  crowdin-pre-push$" | wc -l) > 0 ]]; then
-  echo "==> Check en.json for changes";
+CHANGES="$(git status --porcelain)"
+if [ -n "$CHANGES" ]; then
+  echo "==> Stash local changes"
+  git stash --quiet -u
+fi
+
+if [ -n "$(npm run | grep '^ *crowdin-pre-push$')" ]; then
+  echo "==> Check en.json for changes"
   npm run crowdin-pre-push
 fi
 
-if [[ ! -z "$CHANGES" ]];
-then
-  echo "==> Apply stashed local changes";
-  git stash pop --quiet;
-fi;
+if [ -n "$CHANGES" ]; then
+  echo "==> Apply stashed local changes"
+  git stash pop --quiet
+fi
 
-exit 0;
+exit 0
--- setup-hooks.sh
+++ setup-hooks.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
 YELLOW='\033[0;33m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-if [[ -d .git ]]; then
-  for hook in pre-push pre-commit post-merge
-  do
-    rm "./.git/hooks/${hook}"
+if [ -d .git ]; then
+  for hook in pre-push pre-commit post-merge; do
+    rm -f "./.git/hooks/${hook}"
     cp "./node_modules/ng-git-hooks/hooks/${hook}.sh" "./.git/hooks/${hook}"
   done
+
   echo -e "${GREEN}Success adding git hooks${NC}"
 else
   echo -e "${YELLOW}Failure adding git hooks: .git/ directory not found${NC}"
-fi;
+fi
